d90c8708e6367b3e043b20ee3a8922edc67a5cf2
teacherMaik <d.michaelthomasbennett@gmail.com>
2024-02-26 01:08:21 +0100

    Wow, page hero is the bomb and first project redone with bootstrap and a touch of personal CSS. Underestimated it's powah!

--- index.js
+++ index.js
@@ -7,68 +7,122 @@ const projectMenus = document.querySelectorAll('.project-menu')
 
 const projectButtons = document.querySelectorAll('.project-menu .btn');
 
-const drape = document.querySelector('.drape');
+const firstTenButtons = document.querySelectorAll('#first-ten .btn');
+
+const secondTenButtons = document.querySelectorAll('#second-ten .btn');
+
+const thirdTenButtons = document.querySelectorAll('#third-ten .btn');
+
+const fourthTenButtons = document.querySelectorAll('#fourth-ten .btn');
+
+const fifthTenButtons = document.querySelectorAll('#fifth-ten .btn');
+
+
+// sleep Function for pause. Can be used for different times by modifying the time input
+function sleep(ms) {
+  return new Promise ((resolve) => setTimeout(resolve, ms));
+};
+
+// Checks to see if typewriter div is in viewport
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+const displayButtons = async (buttons) => {
+
+  let i = 0
+  while (i < buttons.length) {
+
+    console.log(i);
+    buttons[i].classList.remove('invisible');
+    buttons[i].classList.add('visible');
+    await sleep(42);
+    i++;
+  }
+};
+
+const removeButtons = async (buttons)=> {
+
+  let i = 0
+  while (i < buttons.length) {
+
+    console.log(i);
+    buttons[i].classList.remove('visible');
+    buttons[i].classList.add('invisible');
+    i++;
+  }
+}
 
 menuButtons.forEach(button => {
   button.addEventListener('click', function(e) {
 
     console.log(this.id);
-    console.log(projectButtons);
-    console.log(projectButtons.length);
 
-    projectMenus.forEach(menu => {menu.classList.add('invisible')});
+    projectMenus.forEach(menu => {
+      
+      menu.classList.remove('visible');
+      menu.classList.add('invisible');
+    });
+
+    removeButtons(projectButtons);
 
     switch (this.id) {
 
       case 'p1-10':
-        drape.classList.add('active');
+        console.log("case 1");
         projectMenus[0].classList.remove('invisible');
-        drape.classList.remove('active');
+        projectMenus[0].classList.add('visible');
+        displayButtons(firstTenButtons);
         break;
 
       case 'p11-20':
-        drape.classList.add('active');
         projectMenus[1].classList.remove('invisible');
-        drape.classList.remove('active');
+        projectMenus[0].classList.add('visible');
+        displayButtons(secondTenButtons);
         break;
 
       case 'p21-30':
-        drape.classList.add('active');
         projectMenus[2].classList.remove('invisible');
-        drape.classList.remove('active');
+        projectMenus[0].classList.add('visible');
+        displayButtons(thirdTenButtons);
         break;
 
       case 'p31-40':
-        drape.classList.add('active');
         projectMenus[3].classList.remove('invisible');
-        drape.classList.remove('active');
+        projectMenus[0].classList.add('visible');
+        displayButtons(fourthTenButtons);
         break;
 
       case 'p41-50':
-        drape.classList.add('active');
         projectMenus[4].classList.remove('invisible');
-        drape.classList.remove('active');
+        projectMenus[0].classList.add('visible');
+        displayButtons(fifthTenButtons);
         break;
     }
-
   });
 });
 
+// Project 1 - Expandisg cards
+const panes = document.querySelectorAll('.p1-pane');
 
-const cards = document.querySelectorAll('.card');
+panes.forEach(pane => {
 
-cards.forEach(card => {
+  pane.addEventListener('click', function(e) {
 
-  card.addEventListener('click', function(e) {
-
-    cards.forEach(card => {card.classList.remove('active')});
-    console.log(this);
+    panes.forEach(pane => {pane.classList.remove('active')});
+    console.log(this.id);
     if (this.classList.contains('active')) {
+
       return;
     } else {
-      this.classList.add('active');
 
+      this.classList.add('active');
     }
   })
-
 });
