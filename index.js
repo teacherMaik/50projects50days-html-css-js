7c71c13b97a9b6339b69edead21ffd568e7bd5c5
teacherMaik <d.michaelthomasbennett@gmail.com>
2024-02-28 19:14:56 +0100

    project 3 done after some major suffering and copy pasta

--- index.js
+++ index.js
@@ -204,4 +204,13 @@ function updateStep() {
   });
 }
 
+// Project 3
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.content-container')
+
+open.addEventListener('click', () => container.classList.add('show-nav'))
+
+close.addEventListener('click', () => container.classList.remove('show-nav'))
+
 
